866236c15d2327fdc33c201027539ee02c0c5b33
yuchu2016 <yu-chu@foxmail.com>
2017-09-26 15:02:48 +0800

    Initial commit

---
+++ src/main/java/com/yuchu/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.yuchu.dao;
+
+import com.yuchu.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
